b5440549cebbe53838bc42ede8ab164f147dcff7
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-11-28 12:53:52 +0530

    data-binding & directives

--- play-with-angular/projects/shop-page/src/app/app.component.ts
+++ play-with-angular/projects/shop-page/src/app/app.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'shop-page';
+  title: string = 'Shop-IT';
+  currentTab: number = 1;
+  products: any = [
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 1000,
+      description: 'This is a laptop',
+      image: 'assets/Laptop.png',
+      isAvailable: true,
+    },
+    // {
+    //   id: 2,
+    //   name: 'Mobile',
+    //   price: 500,
+    //   description: 'This is a mobile',
+    //   image: 'assets/Mobile.png',
+    //   isAvailable: true,
+    // },
+  ];
+
+  addToCart(event: MouseEvent) {
+    console.log('Product added to cart', event);
+  }
+  handleTabChange(event: MouseEvent, tabIndex: number) {
+    this.currentTab = tabIndex;
+  }
+  isTabSelected(tabIndex: number) {
+    return this.currentTab === tabIndex;
+  }
 }
